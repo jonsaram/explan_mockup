727151825e5ca0e2aebfb8f6994b1cb08554d2e6
batomusu <batomusu@naver.com>
2024-02-17 02:35:41 +0900

    실덕관리

--- js/common.js
+++ js/common.js
@@ -118,6 +118,37 @@
           buttonText: "Select date",
         });
     });
+    /*====== 달력 =======*/
+    $( function() {
+        var dateFormat = "mm/dd/yy",
+            from = $( "#from" )
+            .datepicker({
+                defaultDate: "+1w",
+                changeMonth: true,
+                numberOfMonths: 1
+            })
+            .on( "change", function() {
+                to.datepicker( "option", "minDate", getDate( this ) );
+            }),
+            to = $( "#to" ).datepicker({
+                defaultDate: "+1w",
+                changeMonth: true,
+                numberOfMonths: 1
+            })
+            .on( "change", function() {
+                from.datepicker( "option", "maxDate", getDate( this ) );
+            });
+
+        function getDate( element ) {
+            var date;
+            try {
+                date = $.datepicker.parseDate( dateFormat, element.value );
+                } catch( error ) {
+                date = null;
+            }
+            return date;
+        }
+    });
 
     /*====== 레이어 파업 열고 닫기 =======*/
     $(function ($) {
